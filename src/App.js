95f75000d0507d47ea3fb0ed50a9a730957603d8
saiganesh346 <33099460+saiganesh346@users.noreply.github.com>
2024-05-15 08:18:19 +0530

    FaceApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,199 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+//import Clarifai from 'clarifai';
+import ParticlesBg from 'particles-bg';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
-function App() {
+// const app =new Clarifai.App({
+//   apiKey: '71e0d278b8d9495bb9fa29994ad5dd30'
+// })
+
+const returnclarifaiReturnOptions = (imageUrl) => {
+// Your PAT (Personal Access Token) can be found in the Account's Security section
+const PAT = 'b07c12096a22467a8b0b7bdb037351d2';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'saiganesh346';
+const APP_ID = 'Machinelearning';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const IMAGE_URL = imageUrl;
+// To use image bytes, assign its variable   
+// const IMAGE_BYTES_STRING = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCIeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAAoACgDASIAAhEBAxEB/8QAGQAAAgMBAAAAAAAAAAAAAAAAAAYDBQcE/8QAMBAAAQMDAwMDAgQHAAAAAAAAAQIDBAAFEQYSIQcTMTJBURRhCBYikSNScXKhsdH/xAAZAQACAwEAAAAAAAAAAAAAAAAFBgIDBAf/xAAtEQABAwMBBgQHAQAAAAAAAAABAgMRAAQhMQUSE0FRYQaBocEUFiJCcrHR8P/aAAwDAQACEQMRAD8A3+RYY1unSYzCS0ttZUkAgktn0q5yT7jPyDUC4wdGwycH5U2Kt9ZQ7VI1qw5PkvQy3CSVPpf7aQjuKyFH25xzn3pHn3TVNy01Hl2hyy6YdkSpKsS9sl/6RlI3rRu3dxWd6spwnAGPIJTfl925fcLaoSDHXvyo6i9SlCQrU9wKln3OyWiaDN1RAbW3kKbSd7gPtwMkH/tTWy9afuy1iPfnXMAblITwkE4yf08cn3pSbYt1uts24XH6fUbiLAuY1MWyGkLEmUW0rcCRvUpQ5CtwKQCPgi4S1ZbDe4sd9NntDEe79m3uOBLTr0IR9jzodSMqUpTu9JJ8owD7UTT4ZCfv9PbP7860m+s+HBSrejWRuz2kAxoesGYxTW/Zlpkwo1vkuSly3UgKWQUhHJUvIHsAaKTemF8XE6sWmxyZkiaZrMh1jv8ArQNpUVqB8FW0njHqx4zRVVhsph1KlKk5xQ+7uHmikaSJrQerMByet2IwvtuTLa4xv2k7Rk84H9x/esHv92d01boenLXGcuiWrFIhLlpbcaQ2/JdK3VJCkAq2pAR7Zz7YxWudY9fxNIdQbNGkR5TyX4aisNNpUMFZAzkj4NK0jq9ZpbLr0PSlzkhrlZDaQlP3P8Q4/ap3F87bPucJEkx/hHv60b2TYXLrKN5sramYECSQRk9M6c6zmJ+eb5Hi22M7cnWGIQgFLbX0zSo4PDa1YBcTgDyMjJ/qbGPabH08SJt1Uzc9QqRliGg5QySPKvgc+TyfYDmmTUWpNYz7ctxoQdPQshCktupckDJUPUcJT6DwMq8YyaQ9VL0pCS8zapcq4SVOBZmPDO8/cnknlWcDBwn4NYnPjLkQ+qE9OtOVlYpeVHDCEkkkJyT+SuQzy5Y0ru6Ez511/Efa5s1fdkOtyVurIxgdlQAA9gOKKPwolU7remU5hCGYEgo38KUv9I/0TRTDYJCWQBSF4rIN/CRgAR0iTpVD1j1g/qDqJcJqlKcjB9bcda142MpOEJAzgeMnjyTSyze5KEuNRpDoDvC0oe4X9iAeaKKFK+oya6fbOqYbDTeEiAPKpHdS3gBLYc7RQkp3ApQog+cq8nwPJrljzxnPZbUfnugn/NFFRgEVch9xKsH0H8pg6e3x3T3UC1ajaZITGkJLoS4MKbOUrzz/ACKVRRRVzVwtoQmhG1NkWu0HuI+JI8u/Kv/Z';  
+
+const raw = JSON.stringify({
+  "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": IMAGE_URL
+                  // "base64": IMAGE_BYTES_STRING
+              }
+          }
+      }
+  ]
+});
+
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
+
+return requestOptions;
+}
+
+// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+// this will default to the latest version_id
+
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000')
+  //   .then(response => response.json())
+  //   .then(console.log)
+  // }
+
+  // calculateFaceLocation = (data) => {
+  //  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  //  const image = document.getElementById('inputimage');
+  //  const width = Number(image.width);
+  //  const height = Number(image.height);
+  //  console.log(width, height);
+  //  return{
+  //  leftCol: clarifaiFace.left_col * width,
+  //  topRow: clarifaiFace.top_row * height,   
+  //  rightCol: width - (calarifaiFace.right_col * width),
+  //  bottomRow: height - (calarifaiFace.bottom_row * height)
+  //  }
+  // }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange =(event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = (MODEL_ID) => {
+    this.setState({imageUrl: this.state.input});
+    console.log('Click');
+  //    App.models.predict(
+  //      Clarifai.FACE_DETECT_MODEL /*"71ed81597df64ff6b54fa65fe2529637"*/, 
+  //      this.state.input)
+        
+      fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnclarifaiReturnOptions(this.state.input))
+        .then(response => response.json())
+        .then(result => {
+
+          const regions = result.outputs[0].data.regions;
+
+        regions.forEach(region => {
+        // Accessing and rounding the bounding box values
+          const boundingBox = region.region_info.bounding_box;
+          const topRow = boundingBox.top_row.toFixed(3);
+          const leftCol = boundingBox.left_col.toFixed(3);
+          const bottomRow = boundingBox.bottom_row.toFixed(3);
+          const rightCol = boundingBox.right_col.toFixed(3);
+
+          region.data.concepts.forEach(concept => {
+            // Accessing and rounding the concept value
+            const name = concept.name;
+            const value = concept.value.toFixed(4);
+
+            console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+            
+        });
+    });
+
+})
+.catch(error => console.log('error', error));
+    //      .then(response => this.calculateFaceLocation(response);
+     //      .catch(err => console.log(err));
+    //     /*console.log(response.outputs[0].data.regions[0].region_info.bouding_box);*/
+    //    /* //do something with response
+    //   },
+    //   function(err) {
+    //     //there was an error
+    //   }
+    // )*/
+   }
+
+   onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route ==='home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+   }
+  
+  render() {
+    const  {isSignedIn, imageUrl, route, box} = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ParticlesBg type="fountain" bg={true} className='particles'
+      params={{particlesOptions}}
+    />
+    <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+    {route === 'home'
+      ?<div>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition box = {box} imageUrl={imageUrl}/>
+      </div>
+      :(
+        route === 'signin'
+      ?<Signin onRouteChange={this.onRouteChange}/>
+      :<Register onRouteChange={this.onRouteChange}/>
+      )
+    }
     </div>
   );
+}
 }
 
 export default App;
